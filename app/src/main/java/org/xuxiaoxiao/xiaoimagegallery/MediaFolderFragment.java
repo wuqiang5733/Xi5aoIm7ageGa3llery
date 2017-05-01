86731457a78065dc7e6f1146b80ea4674f845d4c
xiaoqianghou <2662651103@qq.com>
2017-05-01 17:27:50 +0800

    全部合并好了

--- app/src/main/java/org/xuxiaoxiao/xiaoimagegallery/MediaFolderFragment.java
+++ app/src/main/java/org/xuxiaoxiao/xiaoimagegallery/MediaFolderFragment.java
@@ -1,5 +1,7 @@
 package org.xuxiaoxiao.xiaoimagegallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -13,7 +15,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.ArrayList;
 
@@ -66,12 +73,13 @@ public class MediaFolderFragment extends Fragment {
         @Override
         protected void onPostExecute(ArrayList<MediaFolderModel> mediaFolderModels) {
 //            super.onPostExecute(mediaFolderModels);
-            if (mediaFolderModels == null) {
-                Toast.makeText(getActivity(), "没有可供选择的图片", Toast.LENGTH_SHORT).show();
-            } else {
+            if (mediaFolderModels == null){
+                Toast.makeText(getActivity(),"没有可供选择的图片",Toast.LENGTH_SHORT).show();
+            }else {
                 mediaAdapter = new MediaFolderAdapter(getActivity(), mediaFolderModels);
                 recyclerView.setAdapter(mediaAdapter);
             }
+
         }
     }
 
@@ -91,9 +99,7 @@ public class MediaFolderFragment extends Fragment {
 //        String orderBy = MediaStore.Images.Media.BUCKET_DISPLAY_NAME;
         String orderBy = MediaStore.Images.Media.DATE_TAKEN;
         cursor = getActivity().getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
-        if (cursor.getCount() == 0) {
-            return null;
-        }
+        if (cursor.getCount() == 0){return null;}
 //        cursor = getContentResolver().query(uri, projection, null, null, orderBy + " DESC");
 
         column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
@@ -155,4 +161,71 @@ public class MediaFolderFragment extends Fragment {
         return mediaFolderModels;
     }
 
+    /**
+     * Created by WuQiang on 2017/4/30.
+     */
+
+    public static class MediaFolderAdapter extends RecyclerView.Adapter<MediaFolderAdapter.MediaViewHolder> {
+        ArrayList<MediaFolderModel> mediaFolderModels = new ArrayList<>();
+
+        Context context;
+
+        public MediaFolderAdapter(Context context, ArrayList<MediaFolderModel> mediaFolderModels) {
+            this.mediaFolderModels = mediaFolderModels;
+            this.context = context;
+        }
+
+        @Override
+        public MediaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.media_floder, parent, false);
+            return new MediaViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MediaViewHolder holder, int position) {
+            String mediaFolderName = mediaFolderModels.get(position).getFolderName();
+            holder.folderName.setText(mediaFolderName);
+            holder.itemSum.setText(String.valueOf(mediaFolderModels.get(position).getMediaItemSum()));
+
+            Glide.with(context).load("file://" + mediaFolderModels.get(position).getFirstItemImage())
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .into(holder.folderImage);
+            holder.bind(mediaFolderName);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mediaFolderModels.size();
+        }
+
+        public class MediaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+            ImageView folderImage;
+            TextView folderName;
+            TextView itemSum;
+            String mediaFolderName;
+
+            public MediaViewHolder(View itemView) {
+                super(itemView);
+                folderImage = (ImageView) itemView.findViewById(R.id.media_floder_image_view);
+                folderName = (TextView) itemView.findViewById(R.id.media_folder_name_text_view);
+                itemSum = (TextView)itemView.findViewById(R.id.media_item_sum_text_view);
+                itemView.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = MediaItemActiviey.newIntent(context,mediaFolderName);
+                context.startActivity(intent);
+            }
+
+
+            public void bind(String mediaFolderName) {
+                this.mediaFolderName = mediaFolderName;
+                //            Log.d("WQWQ",mediaFolderName);
+            }
+        }
+    }
 }
